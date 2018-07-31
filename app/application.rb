1437861b4790b476b5337a5a5f873605d5d87606
Jamie Brown <jmbrowncow@gmail.com>
2018-07-31 21:41:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_req = req.path.split("/items/").last
       item_object = @@items.find{|item| item.name == item_req}
-      binding.pry
+      #binding.pry
       !!item_object ? item_object.price : resp.status = 400
             #resp.write "Item not found"
     else
