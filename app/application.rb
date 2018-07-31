6fe347aee28dcfa4162a5b4db09322ead7197f02
Jamie Brown <jmbrowncow@gmail.com>
2018-07-31 21:42:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,8 +10,12 @@ class Application
       item_req = req.path.split("/items/").last
       item_object = @@items.find{|item| item.name == item_req}
       #binding.pry
-      !!item_object ? item_object.price : resp.status = 400
-            #resp.write "Item not found"
+      if !!item_object 
+        item_object.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Route not found"
